020fa80a789d92569a498fe2c853c466358300fb
RUBEN SOLARES HERNANDEZ <solares.ruben7@gmail.com>
2025-11-10 17:55:27 -0600

    Primer commit hola mundo

---
+++ holaMundo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout<<"Hola Mundo!"<<'\n';
+    std::cout<<"Me gusta la pizza";
+    return 0;
+}
+
+//Esto es un comentario de una sola línea
+
+/*
+Este es un comentario
+de varias 
+líneas
+*/
